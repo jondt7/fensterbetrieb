e15a69cf351ab632e1ced1f3dcb88987c3ab058e
jondt7 <jojoneu7@gmail.com>
2025-08-25 23:00:29 +0200

    tobots.ts geändert

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@
 import type { MetadataRoute } from "next"
 
 export default function robots(): MetadataRoute.Robots {
-  const base = process.env.SITE_URL || "https://www.meilen.de"
+  const base = process.env.SITE_URL || "https://www.meylen.de"
 
   return {
     rules: [
